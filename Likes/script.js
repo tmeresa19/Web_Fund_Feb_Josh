6c2758728f44c6a973e0e98738f4fcb56874ae3c
Tigist Meresa <tmeresa19@gmail.com>
2023-02-21 20:16:43 -0800

    Updated Likes Core assignment's JS code

--- Likes/script.js
+++ Likes/script.js
@@ -1,11 +1,42 @@
 console.log("checking if js works...");
 
-// Solutions for the following 2 questions
+// Solutions
 
 // 1. Each time we click the "Like" button, it should increase the number of likes by "1"
 
-function addLike(element){
-    document.querySelector('#spanid').innerText=element.value++
+// ==========For Neil M =======
+
+var count9 = 9
+var span9 = document.querySelector('#spanid9')
+function addLike9() {
+    count9++
+    span9.innerText = count9
 }
 
-// 2. Clicking each like button will increase the number of likes for each post by "1"
+// ===============For Nichole K=================
+
+var count12 = 12
+var span12 = document.querySelector('#spanid12')
+function addLike12() {
+    count12++
+    span12.innerText = count12
+}
+// ================For Jim R ====================
+
+var count99 = 9
+var span99 = document.querySelector('#spanid99')
+function addLike99() {
+    count99++
+    span99.innerText = count99
+}
+
+
+
+
+var count3 = 3
+var span3 = document.querySelector('#spanid3')
+function addLike3() {
+    count3++
+    span3.innerText = count3
+}
+
